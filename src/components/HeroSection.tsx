3fc6b5855878c5c801ec0e6adbc78f7a1672325a
inepali <sanjayaghimire@gmail.com>
2025-11-04 18:42:33 -0500

    testing code reverted.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -23,7 +23,7 @@ export const HeroSection = () => {
       {/* Content */}
       <div className="relative z-10 text-center text-white px-4 sm:px-6 lg:px-8 max-w-4xl mx-auto">
         <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold mb-6 leading-tight">
-          Wedding Photographers in {location.city}, {location.state} testing sysnc deployment
+          Wedding Photographers in {location.city}, {location.state}
           <span className="block text-gold text-3xl sm:text-4xl lg:text-5xl font-medium mt-2">
             Premier Photography & Videography Services
           </span>
